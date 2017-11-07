b282c16cad411e59c41620366d52d4981e1ddd24
JaysenStark <838459914@qq.com>
2017-11-06 22:08:23 -0800

    update cardtool

--- cardtool.py
+++ cardtool.py
@@ -1,23 +1,29 @@
 
+
 class CardTool(object):
 
-    rank = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'A']
+    rank = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
     suit = ['c', 'd', 'h', 's']
 
     # construct a dict key='ranksuit' value in 0-51
     card_dict ={}
-    # todo
+    for i in range(len(rank)):
+        for j in range(len(suit)):
+            str_temp = rank[i] + suit[j]
+            card_dict[str_temp] = i * len(suit) + j
 
-    string_dict={} # a reverse dict of card_dict
-    # todo
+    # a reverse dict of card_dict
+    string_dict={}
+    for k, v in card_dict.items():
+        string_dict[v] = k
 
     # return int number represent card
     @classmethod
     def string_2_card(cls, string):
-        cls.card_dict[string]
+        return cls.card_dict[string]
 
     # return string represent card
     @classmethod
-    def card_to_string(cls, string):
-        # todo
-        return
+    def card_to_string(cls, value):
+        return cls.string_dict[value]
+
